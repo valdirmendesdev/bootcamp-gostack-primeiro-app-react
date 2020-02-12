25535e5e32837249e03534470b7c1ca9d41ce424
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2020-02-12 08:02:35 -0300

    Project structure

---
+++ src/pages/Main/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from "../../services/api";
+
+import { Container, Form, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  }
+
+  handleInputChage = e => {
+    this.setState({
+      newRepo: e.target.value
+    });
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const { newRepo, repositories } = this.state;
+
+    this.setState({loading: true});
+
+    const response = await api.get(`/repos/${newRepo}`)
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false
+    });
+
+  }
+
+  render() {
+
+    const {newRepo, loading} = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChage} />
+          <SubmitButton
+            loading={loading}>
+
+            { loading ? (<FaSpinner color="#FFF" size={14} />) :
+            (<FaPlus color="#FFF" size={14} />) }
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
